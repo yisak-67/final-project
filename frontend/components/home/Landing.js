e0b8ad81a4d96b3651c3c7191b5d41d70c82a0b9
yisak-67 <yisakzemedu6774@gmail.com>
2025-03-28 20:12:47 +0300

    landing page finshed

---
+++ frontend/components/home/Landing.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Feature from "./Feature";
+import Header from "./Header";
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+function Landing(props) {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Feature />
+      <Footer />
+    </div>
+  );
+}
+
+export default Landing;
