9a9cb5ed97cb4a03bf3a1b4cd4d86bfc6f80fef1
yisak-67 <yisakzemedu6774@gmail.com>
2025-04-01 08:05:46 +0300

    selller dashboared

---
+++ frontend/pages/p_seller/Dashboard.tsx
@@ -0,0 +1,14 @@
+import SellerLayout from "@/layout/SellerLayout";
+import React, { useEffect } from "react";
+
+const Seller_dashboard = () => {
+  return (
+    <SellerLayout>
+      <div className="ml-[270px] h-[93vh] flex justify-center items-center">
+        <p>Dashboard content +</p>
+      </div>
+    </SellerLayout>
+  );
+};
+
+export default Seller_dashboard;
