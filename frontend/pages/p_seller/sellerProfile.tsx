54988dc435ab6d20dd0a0a9f295db5205687084a
yisak-67 <yisakzemedu6774@gmail.com>
2025-04-01 15:43:17 +0300

    seller profile completed

---
+++ frontend/pages/p_seller/sellerProfile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SellerLayout from "@/layout/SellerLayout";
+import ViewProfile from "@/components/profile/viewProfile";
+
+const SellerProfile = () => {
+  return (
+    <SellerLayout>
+      <div className="ml-[270px] h-full">
+        <ViewProfile />
+      </div>
+    </SellerLayout>
+  );
+};
+
+export default SellerProfile;
