1c5cd57c889994af70f3b34a92c9a92b7dd11122
yisak-67 <yisakzemedu6774@gmail.com>
2025-04-07 08:46:58 +0300

    buyer profile  finshed

---
+++ frontend/pages/p_auth/buyerProfile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@/components/admin/layout";
+import ViewProfile from "@/components/profile/viewProfile";
+
+const UserProfile = () => {
+  return (
+    <div className=" h-full">
+      <ViewProfile />
+    </div>
+  );
+};
+
+export default UserProfile;
