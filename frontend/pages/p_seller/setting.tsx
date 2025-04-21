31f9b094227b463d2fb9ff0ce2cc8be11aa30f41
yisak-67 <yisakzemedu6774@gmail.com>
2025-04-21 07:21:26 +0300

    setting

---
+++ frontend/pages/p_seller/setting.tsx
@@ -0,0 +1,15 @@
+import SellerLayout from "@/layout/SellerLayout";
+import React from "react";
+
+const Setting = () => {
+  return (
+    <SellerLayout>
+      <div className="ml-[270px] h-[93vh] flex items-center justify-center">
+        setting
+      </div>
+    </SellerLayout>
+  );
+};
+
+export default Setting;
+ 
