c407acd6f58a7e8c01b4c075a4397bf03a1acd4f
yisak-67 <yisakzemedu6774@gmail.com>
2025-05-02 08:44:45 +0300

    add image componet

--- frontend/components/admin/landImage.tsx
+++ frontend/components/admin/landImage.tsx
@@ -35,7 +35,7 @@ const LandImage = ({
   return (
     <div className="w-[500px] h-full bg-slate-100 border-gray-100 shadow ring-1 ring-gray-50  fixed right-0 top-0 p-6 z-[1000] text-white rounded-tl-[25px]  transition duration-400 ease-in-out    ">
       <div className="flex flex-col  justify-start items-start gap-[#100px]">
-        <button className="mt-1  ">
+        <button className="mt-1" title="Close">
           <AiOutlineLeft className="text-black" size={30} onClick={onclose} />
         </button>
       </div>
